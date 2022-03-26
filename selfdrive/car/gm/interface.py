51b71bf3d0fd9e85788a24e0730453122c1ba749
darknight111 <75706333+darknight111@users.noreply.github.com>
2022-03-26 14:59:28 -0700

    Testing long and lat changes

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -187,20 +187,20 @@ class CarInterface(CarInterfaceBase):
       ret.steerActuatorDelay = 0.
       ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[10., 41.0], [10., 41.0]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.18, 0.275], [0.01, 0.021]]
-      ret.lateralTuning.pid.kf = 0.0002
-      ret.steerMaxBP = [10., 25.]
-      ret.steerMaxV = [1., 1.2]
+      ret.lateralTuning.pid.kf = 0.00022
+      ret.steerMaxBP = [0.]
+      ret.steerMaxV = [1.3]
       
       # TODO: Needs refinement for stop and go, doesn't fully stop
       # Assumes the Bolt is using L-Mode for regen braking
       ret.longitudinalTuning.kpBP = [0., 35]
-      ret.longitudinalTuning.kpV = [0.21, 0.46] 
+      ret.longitudinalTuning.kpV = [0.19, 0.43] 
       ret.longitudinalTuning.kiBP = [0., 35.] 
       ret.longitudinalTuning.kiV = [0.22, 0.33]
       ret.stoppingDecelRate = 0.17  # reach stopping target smoothly, brake_travel/s while trying to stop
-      ret.stopAccel = 0. # Required acceleraton to keep vehicle stationary
-      ret.vEgoStopping = 0.6  # Speed at which the car goes into stopping state, when car starts requesting stopping accel
-      ret.vEgoStarting = 0.6  # Speed at which the car goes into starting state, when car starts requesting starting accel,
+      ret.stopAccel = -3.0 # Required acceleraton to keep vehicle stationary
+      ret.vEgoStopping = 0.7  # Speed at which the car goes into stopping state, when car starts requesting stopping accel
+      ret.vEgoStarting = 0.7  # Speed at which the car goes into starting state, when car starts requesting starting accel,
       # vEgoStarting needs to be > or == vEgoStopping to avoid state transition oscillation
       ret.stoppingControl = True
       ret.longitudinalTuning.deadzoneBP = [0.]
