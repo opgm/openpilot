6f19314e39d8fd5624fd5388b9a2ac9da7ff50f2
Eric Brown <eric@ebrown.net>
2023-02-13 11:13:53 -0700

    Use Volt FF for CC Volt

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -38,7 +38,7 @@ class CarInterface(CarInterfaceBase):
     return 0.04689655 * sigmoid * (v_ego + 10.028217)
 
   def get_steer_feedforward_function(self):
-    if self.CP.carFingerprint == CAR.VOLT:
+    if self.CP.carFingerprint in (CAR.VOLT, CAR.VOLT_CC):
       return self.get_steer_feedforward_volt
     elif self.CP.carFingerprint == CAR.ACADIA:
       return self.get_steer_feedforward_acadia
