17655632d7168cc76429d0a72b1e877ac6ab3d55
FrogAi <91348155+FrogAi@users.noreply.github.com>
2023-08-20 20:30:08 -0700

    Nudgeless lane change + lane detection
    Added toggles for nudgeless lane changes, lane detection, and one lane change per signal activation with a lane change delay factor.

---
+++ selfdrive/ui/qt/offroad/frogpilot_settings.cc
@@ -0,0 +1,201 @@
+#include <QHBoxLayout>
+#include <QWidget>
+#include <tuple>
+#include <vector>
+
+#include "common/params.h"
+#include "selfdrive/ui/qt/offroad/frogpilot_settings.h"
+#include "selfdrive/ui/qt/widgets/input.h"
+#include "selfdrive/ui/ui.h"
+
+FrogPilotControlsPanel::FrogPilotControlsPanel(QWidget *parent) : FrogPilotPanel(parent) {
+  mainLayout = new QVBoxLayout(this);
+
+  QLabel *const descriptionLabel = new QLabel("Click on the toggle names to see a detailed toggle description", this);
+  mainLayout->addWidget(descriptionLabel);
+  mainLayout->addSpacing(25);
+  mainLayout->addWidget(white_horizontal_line());
+
+  static const std::vector<std::tuple<QString, QString, QString, QString>> toggles = {
+    {"DeviceShutdownTimer", "Device Shutdown Timer", "Set the timer for when the device turns off after being offroad to reduce energy waste and prevent battery drain.", "../assets/offroad/icon_time.png"},
+    {"FireTheBabysitter", "Fire the Babysitter", "Disable some of openpilot's 'Babysitter Protocols'.", "../assets/offroad/icon_babysitter.png"},
+    {"NudgelessLaneChange", "Nudgeless Lane Change", "Switch lanes without having to nudge the steering wheel.", "../assets/offroad/icon_lane.png"}
+  };
+
+  for (const auto &[key, label, desc, icon] : toggles) {
+    ParamControl *control = createParamControl(key, label, desc, icon, this);
+    if (key == "DeviceShutdownTimer") {
+      mainLayout->addWidget(new DeviceShutdownTimer());
+      mainLayout->addWidget(horizontal_line());
+    } else if (key == "FireTheBabysitter") {
+      createSubControl(key, label, desc, icon, {}, {
+        {"DisableAllLogging", "Disable Logging", "Prevent all data tracking by comma to go completely incognitio or to even just reduce thermals.\n\nWARNING: This will prevent any drives from being recorded and they WILL NOT be recoverable!"}
+      });
+      createSubButtonControl(key, {
+        {"MuteDM", "Mute DM"},
+        {"MuteDoor", "Mute Door Open"},
+        {"MuteSeatbelt", "Mute Seatbelt"},
+        {"MuteSystemOverheat", "Mute Overheat"}
+      }, mainLayout);
+    } else if (key == "NudgelessLaneChange") {
+      createSubControl(key, label, desc, icon, {
+        new LaneChangeTimer(),
+      });
+      createSubButtonControl(key, {
+        {"LaneDetection", "Lane Detection"},
+        {"OneLaneChange", "One Lane Change Per Signal"},
+      }, mainLayout);
+    } else {
+      mainLayout->addWidget(control);
+      if (key != std::get<0>(toggles.back())) mainLayout->addWidget(horizontal_line());
+    }
+  }
+  setInitialToggleStates();
+}
+
+FrogPilotVisualsPanel::FrogPilotVisualsPanel(QWidget *parent) : FrogPilotPanel(parent) {
+  mainLayout = new QVBoxLayout(this);
+
+  QLabel *const descriptionLabel = new QLabel("Click on the toggle names to see a detailed toggle description", this);
+  mainLayout->addWidget(descriptionLabel);
+  mainLayout->addSpacing(25);
+  mainLayout->addWidget(white_horizontal_line());
+
+  static const std::vector<std::tuple<QString, QString, QString, QString>> toggles = {
+    {"FrogTheme", "FrogPilot Theme", "Enable the beloved FrogPilot Theme! Disable toggle to revert back to the stock openpilot theme.", "../assets/images/frog_button_home.png"},
+    {"NumericalTemp", "Numerical Temperature Gauge", "Replace openpilot's 'GOOD', 'OK', and 'HIGH' temperature statuses with numerical values.\n\nTap the gauge to switch between Celsius and Fahrenheit.", "../assets/offroad/icon_temp.png"},
+    {"ScreenBrightness", "Screen Brightness", "Choose a custom screen brightness level or use the default 'Auto' brightness setting.", "../assets/offroad/icon_light.png"},
+    {"Sidebar", "Sidebar Shown By Default", "Sidebar is shown by default while onroad as opposed to hidden.", "../assets/offroad/icon_metric.png"},
+    {"SilentMode", "Silent Mode", "Disables all openpilot sounds for a completely silent experience.", "../assets/offroad/icon_mute.png"},
+    {"WideCameraDisable", "Wide Camera Disabled (Cosmetic Only)", "Disable the wide camera view. This toggle is purely cosmetic and will not affect openpilot's use of the wide camera.", "../assets/offroad/icon_camera.png"}
+  };
+
+  for (const auto &[key, label, desc, icon] : toggles) {
+    ParamControl *control = createParamControl(key, label, desc, icon, this);
+    if (key == "FrogTheme") {
+      mainLayout->addWidget(control);
+      mainLayout->addWidget(horizontal_line());
+      createSubButtonControl(key, {
+        {"FrogColors", "Colors"},
+        {"FrogIcons", "Icons"},
+        {"FrogSounds", "Sounds"},
+      }, mainLayout);
+    } else if (key == "ScreenBrightness") {
+      mainLayout->addWidget(new ScreenBrightness());
+      mainLayout->addWidget(horizontal_line());
+    } else {
+      mainLayout->addWidget(control);
+      if (key != std::get<0>(toggles.back())) mainLayout->addWidget(horizontal_line());
+    }
+  }
+  setInitialToggleStates();
+}
+
+ParamControl *FrogPilotPanel::createParamControl(const QString &key, const QString &label, const QString &desc, const QString &icon, QWidget *parent) {
+  ParamControl *control = new ParamControl(key, label, desc, icon);
+  connect(control, &ParamControl::toggleFlipped, [=](bool state) {
+    static const QMap<QString, QString> parameterWarnings = {
+    };
+    if (parameterWarnings.contains(key) && params.getBool(key.toStdString())) {
+      ConfirmationDialog::toggleAlert("WARNING: " + parameterWarnings[key], "I understand the risks.", parent);
+    }
+    if (ConfirmationDialog::toggle("Reboot required to take effect.", "Reboot Now", parent)) {
+      Hardware::reboot();
+    }
+    auto it = childControls.find(key.toStdString());
+    if (it != childControls.end()) {
+      for (QWidget *widget : it->second) {
+        widget->setVisible(state);
+      }
+    }
+  });
+  return control;
+}
+
+QFrame *FrogPilotPanel::horizontal_line(QWidget *parent) const {
+  QFrame *line = new QFrame(parent);
+  line->setFrameShape(QFrame::StyledPanel);
+  line->setStyleSheet(R"(
+    border-width: 1px;
+    border-bottom-style: solid;
+    border-color: gray;
+  )");
+  line->setFixedHeight(2);
+  return line;
+}
+
+QFrame *FrogPilotPanel::white_horizontal_line(QWidget *parent) const {
+  QFrame *line = new QFrame(parent);
+  line->setFrameShape(QFrame::StyledPanel);
+  line->setStyleSheet(R"(
+    border-width: 1px;
+    border-bottom-style: solid;
+    border-color: white;
+  )");
+  line->setFixedHeight(2);
+  return line;
+}
+
+QWidget *FrogPilotPanel::createDualParamControl(ParamValueControl *control1, ParamValueControl *control2) {
+  QWidget *mainControl = new QWidget(this);
+  QHBoxLayout *layout = new QHBoxLayout();
+  layout->addWidget(control1);
+  layout->addStretch();
+  layout->addWidget(control2);
+  mainControl->setLayout(layout);
+  return mainControl;
+}
+
+QWidget *FrogPilotPanel::addSubControls(const QString &parentKey, QVBoxLayout *layout, const std::vector<std::tuple<QString, QString, QString>> &controls) {
+  QWidget *mainControl = new QWidget(this);
+  mainControl->setLayout(layout);
+  mainLayout->addWidget(mainControl);
+  mainControl->setVisible(params.getBool(parentKey.toStdString()));
+  for (const auto &[key, label, desc] : controls) addControl(key, "   " + label, desc, layout);
+  return mainControl;
+}
+
+void FrogPilotPanel::addControl(const QString &key, const QString &label, const QString &desc, QVBoxLayout *layout, const QString &icon) {
+  layout->addWidget(createParamControl(key, label, desc, icon, this));
+  layout->addWidget(horizontal_line());
+}
+
+void FrogPilotPanel::createSubControl(const QString &key, const QString &label, const QString &desc, const QString &icon, const std::vector<QWidget*> &subControls, const std::vector<std::tuple<QString, QString, QString>> &additionalControls) {
+  ParamControl *control = createParamControl(key, label, desc, icon, this);
+  mainLayout->addWidget(control);
+  mainLayout->addWidget(horizontal_line());
+  QVBoxLayout *subControlLayout = new QVBoxLayout();
+  for (QWidget *subControl : subControls) {
+    subControlLayout->addWidget(subControl);
+    subControlLayout->addWidget(horizontal_line());
+  }
+  QWidget *mainControl = addSubControls(key, subControlLayout, additionalControls);
+  connect(control, &ParamControl::toggleFlipped, [=](bool state) { mainControl->setVisible(state); });
+}
+
+void FrogPilotPanel::createSubButtonControl(const QString &parentKey, const std::vector<QPair<QString, QString>> &buttonKeys, QVBoxLayout *subControlLayout) {
+  QHBoxLayout *buttonsLayout = new QHBoxLayout();
+  QWidget *line = horizontal_line();
+  buttonsLayout->addStretch();
+  for (const auto &[key, label] : buttonKeys) {
+    FrogPilotButtonParamControl* button = new FrogPilotButtonParamControl(key, label);
+    mainLayout->addWidget(button);
+    buttonsLayout->addWidget(button);
+    buttonsLayout->addStretch();
+    button->setVisible(params.getBool(parentKey.toStdString()));
+    childControls[parentKey.toStdString()].push_back(button);
+  }
+  subControlLayout->addLayout(buttonsLayout);
+  line = horizontal_line();
+  mainLayout->addWidget(line);
+  childControls[parentKey.toStdString()].push_back(line);
+}
+
+void FrogPilotPanel::setInitialToggleStates() {
+  for (const auto& [key, controlSet] : childControls) {
+    bool state = params.getBool(key);
+    for (QWidget *widget : controlSet) {
+      widget->setVisible(state);
+    }
+  }
+}
