4ee20e0a1aa87822d1800076f42eb6d69e9cb33e
Eric Brown <eric@ebrown.net>
2024-07-26 12:50:53 -0600

    [Non-ACC] Simplify accelerator position stuff

--- selfdrive/car/gm/values.py
+++ selfdrive/car/gm/values.py
@@ -250,7 +250,6 @@ class GMFlags(IntFlag):
   PEDAL_LONG = 1
   CC_LONG = 2
   NO_CAMERA = 4
-  NO_ACCELERATOR_POS_MSG = 8
 
 
 # In a Data Module, an identifier is a string used to recognize an object,
