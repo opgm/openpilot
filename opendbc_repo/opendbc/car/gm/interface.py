7240138963ccb7a8d63d31b2388c8ee12bff27c8
Eric Brown <eric@ebrown.net>
2024-08-21 15:04:11 -0600

    [No Camera] Fix panda flag

--- opendbc_repo/opendbc/car/gm/interface.py
+++ opendbc_repo/opendbc/car/gm/interface.py
@@ -272,5 +272,10 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.kiBP = [10.7, 10.8, 28.]
       ret.longitudinalTuning.kiV = [0., 20., 20.]  # set lower end to 0 since we can't drive below that speed
 
+    # Exception for flashed cars, or cars whose camera was removed
+    if (ret.networkLocation == NetworkLocation.fwdCamera or candidate in CC_ONLY_CAR) and CAM_MSG not in fingerprint[
+      CanBus.CAMERA] and not candidate in SDGM_CAR:
+      ret.flags |= GMFlags.NO_CAMERA.value
+      ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_NO_CAMERA
 
     return ret
