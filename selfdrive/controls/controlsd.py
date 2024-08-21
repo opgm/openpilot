24980953609e56929a6f4455700f08d466e314ac
Eric Brown <eric@ebrown.net>
2024-08-21 14:25:06 -0600

    CC longitudinal control

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -108,7 +108,8 @@ class Controls:
     pid_accel_limits = self.CI.get_pid_accel_limits(self.CP, CS.vEgo, CS.vCruise * CV.KPH_TO_MS)
     actuators.accel = self.LoC.update(CC.longActive, CS, long_plan.aTarget, long_plan.shouldStop, pid_accel_limits)
 
-    # Steering PID loop and lateral MPC
+    if len(long_plan.speeds):
+        actuators.speed = long_plan.speeds[-1]# Steering PID loop and lateral MPC
     self.desired_curvature = clip_curvature(CS.vEgo, self.desired_curvature, model_v2.action.desiredCurvature)
     actuators.curvature = self.desired_curvature
     actuators.steer, actuators.steeringAngleDeg, lac_log = self.LaC.update(CC.latActive, CS, self.VM, lp,
