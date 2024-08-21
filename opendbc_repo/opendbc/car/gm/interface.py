9f294172423ae615d71c99701b31ffb6cd1c960f
Eric Brown <eric@ebrown.net>
2024-08-21 14:31:14 -0600

    Pedal interceptor SnG

--- opendbc_repo/opendbc/car/gm/interface.py
+++ opendbc_repo/opendbc/car/gm/interface.py
@@ -239,6 +239,7 @@ class CarInterface(CarInterfaceBase):
         ret.longitudinalTuning.kf = 0.15
         ret.stoppingDecelRate = 0.8
       else:  # Pedal used for SNG, ACC for longitudinal control otherwise
+        ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_HW_CAM_LONG
         ret.startingState = True
         ret.vEgoStopping = 0.25
         ret.vEgoStarting = 0.25
