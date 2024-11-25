0f0a2a9f6c16790a662e5b1da44fb763833084c9
Eric Brown <eric@ebrown.net>
2024-11-24 21:55:25 -0700

    [SnG] Remove line

--- opendbc_repo/opendbc/car/gm/values.py
+++ opendbc_repo/opendbc/car/gm/values.py
@@ -123,7 +123,6 @@ class CAR(Platforms):
   CHEVROLET_VOLT = GMASCMPlatformConfig(
     [GMCarDocs("Chevrolet Volt 2017-18", min_enable_speed=0, video_link="https://youtu.be/QeMCN_4TFfQ")],
     GMCarSpecs(mass=1607, wheelbase=2.69, steerRatio=17.7, centerToFrontRatio=0.45, tireStiffnessFactor=0.469, minEnableSpeed=-1),
-    dbc_dict=dbc_dict('gm_global_a_powertrain_volt', 'gm_global_a_object', chassis_dbc='gm_global_a_chassis')
   )
   CADILLAC_ATS = GMASCMPlatformConfig(
     [GMCarDocs("Cadillac ATS Premium Performance 2018")],
